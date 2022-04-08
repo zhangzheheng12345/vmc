f23ae775795f9899b59cbf777883aab567da8314
zhangzheheng12345@163.com <zhangzheheng12345@163.com>
2022-04-08 16:21:57 +0800

    Temporary commit

--- vmc.cpp
+++ vmc.cpp
@@ -1,1 +1,15 @@
 #include "vmc.h"
+
+namespace vmc {
+
+template<class T> LinearBox<T>::LinearBox(int size) {
+    data = PtrBox(new T[size]);
+    length = size;
+}
+template<class T> LinearBox<T>::LinearBox(const LinearBox& origin) {
+    data = origin.data;
+    data->Add();
+    length = origin.length;
+}
+
+} // namespace end
--- vmc.h
+++ vmc.h
@@ -1,7 +1,47 @@
 #pragma once
 
-class Vec {
+namespace vmc {
+
+template<class T> class PtrBox {
+public:
+    PtrBox(T* ptr) {
+        data = ptr;
+    }
+    void Add() {
+        count++;
+    }
+    void Sub() {
+        if(count){
+            count--;
+            if(count == 0) delete data;
+        }
+    }
+    bool Destroyed() {
+        return !count;
+    }
+private:
+    T* data;
+    unsigned int count;
+};
+
+template<class T> class LinearBox {
+public:
+    LinearBox(int);
+    LinearBox(const LinearBox&);
+    ~LinearBox();
+    T& at(unsigned int);
+    unsigned int len();
+private:
+    PtrBox<T>* data;
+    unsigned int length;
+};
+
+template<class T> class Vec {
 public:
     Vec() {}
     ~Vec() {}
+private:
+    LinearBox<T> data;
 };
+
+} // namespace end
